60c90cdff4f9b3c817afcac7dda0f7a0ef36baa0
Michael-wzl <wangziling0405@gmail.com>
2026-01-28 23:11:19 +0800

    Level 0 for W2: Named chatbot Dude and implemented greets and goodbyes.

---
+++ src/main/java/Dude.java
@@ -0,0 +1,14 @@
+public class Dude {
+    public static final String BOT_NAME = "Dude";
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        
+        // Greet the user
+        System.out.println(line);
+        System.out.println(" Hello! I'm " + BOT_NAME + ".");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
