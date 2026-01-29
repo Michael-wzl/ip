8d9b677aa259c611707f58f59f71c4d790e13ef9
Michael-wzl <wangziling0405@gmail.com>
2026-01-29 11:19:22 +0800

    Level1-W3: Echo user inputs and exit when prompted.

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,18 +1,38 @@
+import java.util.Scanner;
+
 /**
  * Represents a chatbot application.
  */
 public class Dude {
     /** The name of the bot. */
     public static final String BOT_NAME = "Dude";
+
     public static void main(String[] args) {
         String line = "____________________________________________________________";
 
-        // Greet the user
-        System.out.println(line);
-        System.out.println(" Hello! I'm " + BOT_NAME + ".");
-        System.out.println(" What can I do for you?");
-        System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Greet the user
+            System.out.println(line);
+            System.out.println(" Hello! I'm " + BOT_NAME + "!");
+            System.out.println(" What can I do for you?");
+            System.out.println(line);
+
+            // Main interaction loop
+            while (true) {
+                String input = scanner.nextLine();
+
+                if (input.equals("bye")) {
+                    System.out.println(line);
+                    System.out.println(" Bye. Hope to see you again soon!");
+                    System.out.println(line);
+                    break;
+                } else {
+                    // Echo the command
+                    System.out.println(line);
+                    System.out.println(" " + input);
+                    System.out.println(line);
+                }
+            }
+        }
     }
 }
