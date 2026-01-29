b727191dc015b2e2c33be87b6de65b075600f50d
Michael-wzl <wangziling0405@gmail.com>
2026-01-29 11:26:01 +0800

    Level2W3: Able to add tasks

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -9,6 +9,8 @@ public class Dude {
 
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         try (Scanner scanner = new Scanner(System.in)) {
             // Greet the user
@@ -21,16 +23,34 @@ public class Dude {
             while (true) {
                 String input = scanner.nextLine();
 
-                if (input.equals("bye")) {
+                switch (input) {
+                case "bye" -> {
                     System.out.println(line);
                     System.out.println(" Bye. Hope to see you again soon!");
                     System.out.println(line);
-                    break;
-                } else {
-                    // Echo the command
+                }
+                case "list" -> {
+                    // Display all stored tasks
                     System.out.println(line);
-                    System.out.println(" " + input);
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
                     System.out.println(line);
+                }
+                default -> {
+                    // Add the task to the list
+                    if (taskCount < 100) {
+                        tasks[taskCount] = input;
+                        taskCount++;
+                        System.out.println(line);
+                        System.out.println(" added: " + input);
+                        System.out.println(line);
+                    } else {
+                        System.out.println(line);
+                        System.out.println(" Sorry, task list is full!");
+                        System.out.println(line);
+                    }
+                }
                 }
             }
         }
