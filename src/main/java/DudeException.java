4e271995f3b88917084f54182558c2e950a31c90
Michael-wzl <wangziling0405@gmail.com>
2026-02-10 14:15:28 +0800

    Add DudeException class and refactor error handling with exceptions.

---
+++ src/main/java/DudeException.java
@@ -0,0 +1,14 @@
+/**
+ * Represents exceptions specific to the Dude chatbot.
+ */
+public class DudeException extends Exception {
+
+    /**
+     * Creates a new DudeException with the given error message.
+     *
+     * @param message The error message describing the exception.
+     */
+    public DudeException(String message) {
+        super(message);
+    }
+}
